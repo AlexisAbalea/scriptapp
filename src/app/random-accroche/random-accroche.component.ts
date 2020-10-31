fabdd3f34c564f56e4c78a50e6532dcfe1907fe8
AlexisAbalea <abaleaalexis@gmail.com>
2020-10-31 15:34:33 +0100

    generateur accroche

--- src/app/random-accroche/random-accroche.component.ts
+++ src/app/random-accroche/random-accroche.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RandomAccrocheComponent implements OnInit {
 
+  accroche;
+
+  listeAccroches = ['Wesh', 'bien ou quoi ?', 'Salut', 'Tu veux devenir riche ?', 'Salut, tu aimes les patates ?', 'Donnez moi de la moula', 'Salut jeune entrepreneur', 'Tu veux la vie de rêve ?', 'Revenu passif rapidemment'];
   constructor() { }
 
   ngOnInit(): void {
+    this.getAccroche();
+  }
+
+  getAccroche() {
+    const index = Math.floor(Math.random() * Math.floor(this.listeAccroches.length));
+    this.accroche = this.listeAccroches[index];
   }
 
 }
